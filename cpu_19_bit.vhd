85680bd72b327e6009b6ec599ed90436fbe82034
rutvikvasava <65145567+rutvikvasava@users.noreply.github.com>
2024-11-05 17:17:49 +0530

    Update cpu_19_bit.vhd

--- cpu_19_bit.vhd
+++ cpu_19_bit.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 entity cpu is
 port(
@@ -54,6 +55,14 @@ for i in 0 to 7 loop
  mem_sig(test+i)<=mem_sig(src+i) xor key;
 end loop;
 end procedure;
+--fft
+procedure fft(test:in integer;src:in integer) is
+begin
+for i in 0 to 7 loop
+	mem_sig(test+i)<=std_logic_vector(unsigned(mem_sig(src+i))+1);
+end loop;
+end procedure;
+
 
 begin
 if rst='1' then
@@ -107,6 +116,7 @@ end if;
 	-- special op
 		when "10011"=>enc(to_integer(unsigned(r1)),to_integer(unsigned(r2)));
 		when "10100"=>dec(to_integer(unsigned(r1)),to_integer(unsigned(r2)));
+		when "10101"=>fft(to_integer(unsigned(r1)),to_integer(unsigned(r2)));
 	when others=> null;
 end case;
 	pc<=std_logic_vector(unsigned(pc) +1);
